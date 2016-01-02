f08e456e9c3b81bbda1d879b2904f2b6dc2a47c8
Yaroslav Arsenkin <majesticy@gmail.com>
2016-01-02 18:30:49 +0200

    Initial Commit

---
+++ ARSLineProgress/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARSLineProgress
+//
+//  Created by Yaroslav Arsenkin on 02.01.16.
+//  Copyright © 2016 Iaroslav Arsenkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
